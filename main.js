be96990cbf13c87663bd18e6e72da208781cf7ff
Bonello <geno.bonello@gmail.com>
2023-06-28 09:50:17 -0700

    hooked up the api

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 let weather = {
     "apiKey": "5ef8df34b119e7453728c4d5b53d19b2",
-    fetchWeather: function(){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q={sacramento}&units=imperial&appid={5ef8df34b119e7453728c4d5b53d19b2}").then((response) => response.json()).then((data) => console.log(data))
+    fetchWeather: function(city){
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=imperial&appid=" 
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data)) 
+    },
+    displayWeather: function(data){
+        const {name} = data
+        const {icon, description} = data.weather[0]
+        const {temp, humidity} = data.main
+        const {speed} = data.wind
+        console.log(name,icon,description,temp,humidity,speed)
     }
 }
 
-weather.fetchWeather()
+weather.fetchWeather("sacramento")
